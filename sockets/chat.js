b8cd1cc5b566f2610ea321626c6a323a2c0863c9
Eymard <eymardneto115@gmail.com>
2020-11-18 15:06:39 -0300

    ups

--- sockets/chat.js
+++ sockets/chat.js
@@ -6,26 +6,34 @@ module.exports = (io) => {
   sockets.on("connection", (client) => {
     const session = client.request.session;
     const usuario = session.usuario;
+
     client.on("join", (sala) => {
-      if (sala) {
-        sala = sala.replace("?", "");
-      } else {
-        let timestamp = new Date().toString();
-        let md5 = crypto.createHash("md5");
-        sala = md5.update(timestamp).digest("hex");
-      }
-      client.set("sala", sala);
-      client.join(sala);
+      console.log(sala);
+      // if (sala) {
+      //   sala = sala.replace("?", "");
+      // } else {
+      //   let timestamp = new Date().toString();
+      //   let md5 = crypto.createHash("md5");
+      //   sala = md5.update(timestamp).digest("hex");
+      // }
+      // client.set("sala", sala);
+      // client.join(sala);
     });
-    client.on("disconnect", () => {
-      client.get("sala", (erro, sala) => {
-        client.leave(sala);
-      });
+
+    client.on("disconnect", (sala) => {
+      console.log(client.rooms);
+      // client.get("sala", (erro, sala) => {
+      //   client.leave(sala);
+      // });
     });
+
     client.on("send-server", (data) => {
       let msg = "<br>" + usuario.usuario.nome + ":</br>" + data + "<br/>";
-      client.emit("send-client", msg);
-      client.broadcast.emit("send-client", msg);
+      client.get("sala", (erro, sala) => {
+        let data = { email: usuario.usuario.email, sala: sala };
+        client.broadcast.emit("send-client", data);
+        sockets.in(sala).emit("send-client", msg);
+      });
     });
   });
 };
